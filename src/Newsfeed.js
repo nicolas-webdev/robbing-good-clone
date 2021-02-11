c262a28379f5daa87149094fa6957413bc13d220
Nicolas <nicobsas+webdev@gmail.com>
2021-02-12 03:22:58 +0900

    working version

--- src/Newsfeed.js
+++ src/Newsfeed.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Newsfeed.css";
 import LineGraph from "./LineGraph";
+import Timeline from "./Timeline";
+import Chip from "@material-ui/core/Chip";
+import { Avatar } from "@material-ui/core";
 
 const Newsfeed = () => {
+  const [popularTopics, setTopics] = useState([
+    "Technology",
+    "Top Movies",
+    "Upcoming Earnings",
+    "Crypto",
+    "Cannabis",
+    "Healthcare Supplies",
+    "Index ETFs",
+    "Technology",
+    "China",
+    "Pharma",
+  ]);
   return (
     <div className="newsfeed">
       <div className="newsfeed__container">
@@ -13,6 +28,37 @@ const Newsfeed = () => {
           </div>
           <div className="newsfeed__chart">
             <LineGraph />
+            <Timeline />
+          </div>
+        </div>
+        <div className="newsfeed__buying__section">
+          <h2>Buying Power</h2>
+          <h2>$4.11</h2>
+        </div>
+        <div className="newsfeed__market__section">
+          <div className="newsfeed__market__box">
+            <p>Market's Closed</p>
+            <h1>Happy Chinese New Year</h1>
+          </div>
+        </div>
+        <div className="newsfeed__popularlists__section">
+          <div className="newsfeed__popularlists__intro">
+            <h1>Popular lists</h1>
+            <p>Show More</p>
+          </div>
+          <div className="newsfeed__popularlists__badges">
+            {popularTopics.map((topic) => (
+              <Chip
+                className="topic__badge"
+                variant="outlined"
+                label={topic}
+                avatar={
+                  <Avatar
+                    src={`https://avatars.dicebear.com/api/human/${topic}.svg`}
+                  />
+                }
+              />
+            ))}
           </div>
         </div>
       </div>
